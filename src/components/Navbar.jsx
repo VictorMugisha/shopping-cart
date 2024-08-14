8f35159daa5f1ead8672220e4bd5e9a98bce239e
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-14 12:43:41 +0200

    Installed tailwind and added some code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import { FaCartShopping } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <header>
+            <div>
+                <h2>Fake Shop</h2>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/">All Products</Link>
+                    </li>
+                    <li>
+                        <Link to="/">Cars</Link>
+                    </li>
+                    <li>
+                        <Link to="/">Clothings</Link>
+                    </li>
+                    <li>
+                        <Link to="/">Shoes</Link>
+                    </li>
+                </ul>
+                <FaCartShopping />
+            </nav>
+        </header>
+    )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import { Outlet } from 'react-router-dom'
 
 export default function HomePage() {
-  return (
-    <div>HomePage</div>
-  )
+    return (
+        <main>
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </main>
+    )
 }
