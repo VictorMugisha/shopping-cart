81a395d8fa769e255894a47050c5b41d23c3d22b
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-14 14:42:50 +0200

    Made form elements controlled components

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,17 +1,47 @@
+import { nanoid } from "nanoid"
+import { useState } from "react"
 
 export default function NewProduct() {
+    const [productData, setProductData] = useState({
+        productId: '',
+        productTitle: '',
+        productType: '',
+        productMaxPrice: 0,
+        productPrice: 0,
+        productDiscription: ''
+    })
+
+    function handleChange(event) {
+        const { name, value, type } = event.target
+        setProductData(currentState => {
+            return {
+                ...currentState,
+                productId: nanoid(),
+                [name]: value
+            }
+        })
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log(productData)
+    }
+
     return (
         <div className="w-full py-5 px-5 md:px-20">
             <h2 className="text-2xl font-semibold mb-8">Add New Product to the Store</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                     {/* Input box card */}
                     <div>
                         <p className="text-sm font-medium mb-2">Product Title</p>
                         <div>
-                            <input 
-                                type="text" 
-                                placeholder="Enter Product Title" 
+                            <input
+                                type="text"
+                                name="productTitle"
+                                value={productData.productTitle}
+                                onChange={handleChange}
+                                placeholder="Enter Product Title"
                                 className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:border-blue-300"
                             />
                         </div>
@@ -20,9 +50,12 @@ export default function NewProduct() {
                     <div>
                         <p className="text-sm font-medium mb-2">Product Type</p>
                         <div>
-                            <input 
-                                type="text" 
-                                placeholder="Provide Product Type" 
+                            <input
+                                type="text"
+                                name="productType"
+                                value={productData.productType}
+                                onChange={handleChange}
+                                placeholder="Provide Product Type"
                                 className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:border-blue-300"
                             />
                         </div>
@@ -31,9 +64,12 @@ export default function NewProduct() {
                     <div>
                         <p className="text-sm font-medium mb-2">Product Max Price</p>
                         <div>
-                            <input 
-                                type="text" 
-                                placeholder="Type Product Maximum Price" 
+                            <input
+                                type="number"
+                                name="productMaxPrice"
+                                value={productData.productMaxPrice}
+                                onChange={handleChange}
+                                placeholder="Type Product Maximum Price"
                                 className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:border-blue-300"
                             />
                         </div>
@@ -42,9 +78,12 @@ export default function NewProduct() {
                     <div>
                         <p className="text-sm font-medium mb-2">Product Price</p>
                         <div>
-                            <input 
-                                type="text" 
-                                placeholder="Type Product Price" 
+                            <input
+                                type="number"
+                                name="productPrice"
+                                value={productData.productPrice}
+                                onChange={handleChange}
+                                placeholder="Type Product Price"
                                 className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:border-blue-300"
                             />
                         </div>
@@ -52,21 +91,24 @@ export default function NewProduct() {
                     {/* Input box card */}
                     <div className="md:col-span-2 lg:col-span-3">
                         <p className="text-sm font-medium mb-2">Product Description</p>
-                        <textarea 
+                        <textarea
                             placeholder="Give the description of the product"
+                            name="productDiscription"
+                            value={productData.productDiscription}
+                            onChange={handleChange}
                             className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:border-blue-300 h-24 resize-none"
                         ></textarea>
                     </div>
                 </div>
                 <div className="flex justify-end mt-6 gap-4">
-                    <button 
-                        type="reset" 
+                    <button
+                        type="reset"
                         className="py-2 px-4 bg-gray-200 rounded shadow-sm hover:bg-gray-300 focus:outline-none"
                     >
                         Reset
                     </button>
-                    <button 
-                        type="submit" 
+                    <button
+                        type="submit"
                         className="py-2 px-4 bg-blue-500 text-white rounded shadow-sm hover:bg-blue-600 focus:outline-none"
                     >
                         Save
