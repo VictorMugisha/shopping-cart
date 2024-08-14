ae6efda26a66be77bc0b2027344dad9b4382be4c
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-14 17:23:36 +0200

    Added light/dark toggle to the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,29 +2,34 @@ import { FaCartShopping } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { changeTheme } from "../store/theme";
+import { useEffect, useRef } from "react";
 
 export default function Navbar() {
     const currentTheme = useSelector(state => state.theme.value)
     const themeDispatch = useDispatch()
+    const headerRef = useRef(null)
+    const toggleButtonRef = useRef(null)
 
-    function handleToggleTheme(event) {
-        const button = event.target;
-        button.classList.toggle("left-1");
-        button.classList.toggle("right-1");
+    useEffect(() => {
+        const { current } = headerRef
+        const { classList } = current
+        classList.toggle("bg-gray-800")
+        classList.toggle("text-white")
+        toggleButtonRef.current.classList.toggle("right-1")
+        toggleButtonRef.current.classList.toggle("left-1")
+        toggleButtonRef.current.classList.toggle("bg-white")
+    }, [currentTheme])
+
+    function handleToggleTheme() {
         if (currentTheme === "light") themeDispatch(changeTheme("dark"))
         if (currentTheme === "dark") themeDispatch(changeTheme("light"))
     }
-    let dynamicStyles = {}
-    if (currentTheme === "light") {
-        dynamicStyles.backgroundColor = "white"
-        dynamicStyles.color = "black"
-    } else {
-        dynamicStyles.backgroundColor = "black"
-        dynamicStyles.color = "white"
-    }
 
     return (
-        <header className="w-full py-5 px-5 md:px-20 flex items-center justify-between shadow-md" style={dynamicStyles}>
+        <header
+            ref={headerRef}
+            className="w-full py-5 px-5 md:px-20 flex items-center justify-between shadow-md"
+        >
             <div>
                 <Link to="/">
                     <h2 className="font-semibold text-2xl md:text-3xl select-none cursor-pointer">
@@ -42,9 +47,12 @@ export default function Navbar() {
                     </li>
                 </ul>
                 <div className="theme">
-                    <div className="relative w-12 h-6 md:w-16 md:h-7 bg-gray-400 rounded-3xl cursor-pointer">
+                    <div
+                        className="relative w-12 h-6 md:w-16 md:h-7 bg-gray-400 rounded-3xl cursor-pointer"
+                    >
                         <button
-                            className="absolute top-1 bottom-1 right-1 rounded-full bg-white w-6 md:w-8 outline-none"
+                            ref={toggleButtonRef}
+                            className="absolute top-1 bottom-1 right-1 rounded-full bg-gray-700 w-6 md:w-8 outline-none"
                             onClick={handleToggleTheme}
                         ></button>
                     </div>
@@ -53,7 +61,7 @@ export default function Navbar() {
                     <div className="relative">
                         <span
                             className="absolute text-white bg-red-500 rounded-full h-4 w-4 md:h-5 md:w-5 flex items-center justify-center top-[-7px] md:top-[-10px] right-[-7px] md:right-[-10px]"
-                        >9</span>
+                        >0</span>
                         <FaCartShopping className="text-xl md:text-2xl cursor-pointer" />
                     </div>
                 </Link>
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -6,16 +6,8 @@ import { useSelector } from 'react-redux'
 
 export default function Layout() {
     const currentTheme = useSelector(state => state.theme.value)
-    let dynamicStyles = {}
-    if (currentTheme === "light") {
-        dynamicStyles.backgroundColor = "white"
-        dynamicStyles.color = "black"
-    } else {
-        dynamicStyles.backgroundColor = "black"
-        dynamicStyles.color = "white"
-    }
     return (
-        <main className="min-h-screen flex flex-col justify-between" style={dynamicStyles}>
+        <main className="min-h-screen flex flex-col justify-between">
             <div>
                 <Navbar />
                 <Outlet />
